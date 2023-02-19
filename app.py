04b88badd9f7d7d750a541253d523ac034e9af16
dla216 <danny_liu_5@sfu.ca>
2023-02-18 23:57:06 -0800

    update call to api

--- app.py
+++ app.py
@@ -1,51 +1,13 @@
 import os
 import random
 
+import requests
+
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
-import random
-
-def add_french_accents(word):
-    """
-    Adds random French accents to a word
-    """
-    # accents = ['é', 'è', 'ê', 'ë', 'à', 'â', 'î', 'ï', 'ô', 'ù', 'û']
-    vowels = ['a', 'e', 'i', 'o', 'u']
-
-    vowel_dict = {
-        "a": ['à', 'â'],
-        "e": ['é', 'è', 'ê', 'ë'],
-        "i": ['î', 'ï'],
-        "o": ['ô'],
-        "u": ['ù', 'û']
-    }
-
-    accented_word = ''
-    for letter in word:
-        if letter in vowels:
-            letter = random.choice(vowel_dict[letter])
-            accented_word += letter
-        else:
-            accented_word += letter
-    if (accented_word[len(accented_word) - 1] == 'n'):
-        accented_word += 'é'
-    return accented_word
-
-def translate_to_french(text):
-    """
-    Translates an English string to French
-    """
-    words = text.split()
-    translated_words = []
-    for word in words:
-        accented_word = add_french_accents(word)
-        translated_words.append(accented_word)
-    translated_text = ' '.join(translated_words)
-    return translated_text
 
 
 @app.route("/", methods=("GET", "POST"))
@@ -57,12 +19,14 @@ def index():
             prompt=generate_prompt(animal),
             temperature=0.6,
         )
-        return redirect(url_for("index", result=translate_to_french(response.choices[0].text)))
+        url = 'http://localhost:8080/translate'
+        params = {'name': response.choices[0].text}
+
+        response = requests.get(url, params=params)    
+        return redirect(url_for("index", result=response.content))
 
     result = request.args.get("result")
-    # return result
     return render_template("index.html", result=result)
-
 
 
 def generate_prompt(animal):
